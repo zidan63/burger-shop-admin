17e95571a7116c16a9f4840e145f156517b42629
Phu Thinh <thinhcp@ggroup.vn>
2023-11-26 00:40:38 +0700

    fix: type

--- src/components/order/OrderToolbar.tsx
+++ src/components/order/OrderToolbar.tsx
@@ -1,32 +1,5 @@
 import HeaderCustom from "@components/_common/HeaderCustom/HeaderCustom";
-import AddIcon from "@mui/icons-material/Add";
-import { Box, Button } from "@mui/material";
-import { useAppDispatch } from "@store";
-import { CategoryActions } from "@store/category";
 
 export const OrderToolbar = () => {
-  const dispatch = useAppDispatch();
-  const handleAdd = () => {
-    dispatch(
-      CategoryActions.setForm({
-        open: true,
-        category: null,
-      })
-    );
-  };
-
-  return (
-    <HeaderCustom title="Quản lý đơn hàng">
-      {/* <Box>
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={() => handleAdd()}
-          startIcon={<AddIcon />}
-        >
-          Thêm đơn hàng
-        </Button>
-      </Box> */}
-    </HeaderCustom>
-  );
+  return <HeaderCustom title="Quản lý đơn hàng" />;
 };
